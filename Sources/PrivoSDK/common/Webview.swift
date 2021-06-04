ec6d311c38ef36b3675984ba155ee65ecbcb923d
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-06-04 13:33:16 +0300

    Improve js events handling in WebView

--- Sources/PrivoSDK/common/Webview.swift
+++ Sources/PrivoSDK/common/Webview.swift
@@ -4,36 +4,29 @@ import WebKit
 struct Webview: UIViewRepresentable {
     
     let url: URL
-    let contentController = ContentController()
 
     func makeUIView(context: UIViewRepresentableContext<Webview>) -> WKWebView {
         let webview = WKWebView()
-        webview.configuration.userContentController = contentController
         let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
         webview.load(request)
-
         return webview
     }
 
     func updateUIView(_ webview: WKWebView, context: UIViewRepresentableContext<Webview>) {
+        let contentController = ContentController()
         let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
+        webview.configuration.userContentController.add(contentController, name: "test")
         webview.load(request)
     }
     
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        guard let dict = message.body as? [String : AnyObject] else {
-            return
+    class ContentController: WKUserContentController, WKScriptMessageHandler {
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            print(message)
+            guard let dict = message.body as? [String : AnyObject] else {
+                return
+            }
+            print(dict)
         }
-
-        print(dict)
     }
 }
 
-class ContentController: WKUserContentController, WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        guard let dict = message.body as? [String : AnyObject] else {
-            return
-        }
-        print(dict)
-    }
-}
