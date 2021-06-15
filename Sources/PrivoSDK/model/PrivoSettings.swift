45eb7080d77f578ba476d61ffb08cd915b9204ae
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-06-15 14:58:18 +0300

    Change processing of siteIdentifier

--- Sources/PrivoSDK/model/PrivoSettings.swift
+++ Sources/PrivoSDK/model/PrivoSettings.swift
@@ -8,12 +8,10 @@
 public struct PrivoSettings {
     public let serviceIdentifier: String
     public let envType: EnviromentType
-    public let siteIdentifier: String?
     public let apiKey: String?
-    public init(serviceIdentifier: String, envType: EnviromentType, siteIdentifier: String? = nil, apiKey: String? = nil) {
+    public init(serviceIdentifier: String, envType: EnviromentType, apiKey: String? = nil) {
         self.serviceIdentifier = serviceIdentifier
         self.envType = envType
-        self.siteIdentifier = siteIdentifier
         self.apiKey = apiKey
     }
 }
