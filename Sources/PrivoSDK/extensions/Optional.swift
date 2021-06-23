9ba7ddfe01fe7617bc9e2e44753f7bfc23fdc907
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-06-23 16:00:38 +0300

    Make Optional extension public

--- Sources/PrivoSDK/extensions/Optional.swift
+++ Sources/PrivoSDK/extensions/Optional.swift
@@ -5,4 +5,21 @@
 //  Created by alex slobodeniuk on 23.06.2021.
 //
 
-import Foundation
+public extension Optional where Wrapped == String {
+    fileprivate var _boundString: String? {
+        get {
+            return self
+        }
+        set {
+            self = newValue
+        }
+    }
+    var boundString: String {
+        get {
+            return _boundString ?? ""
+        }
+        set {
+            _boundString = newValue.isEmpty ? nil : newValue
+        }
+    }
+}
