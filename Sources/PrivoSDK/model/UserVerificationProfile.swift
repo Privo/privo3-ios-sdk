29a90ae28d52c82c585b04e49c0e16aa6117b299
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-06-23 16:21:59 +0300

    Fix Date in UserVerificationProfile

--- Sources/PrivoSDK/model/UserVerificationProfile.swift
+++ Sources/PrivoSDK/model/UserVerificationProfile.swift
@@ -4,16 +4,17 @@
 //
 //  Created by alex slobodeniuk on 14.06.2021.
 //
+import Foundation
 
 public struct UserVerificationProfile: Encodable {
     public var firstName: String?
     public var lastName: String?
-    public var birthDate: String?
+    public var birthDate: Date?
     public var email: String?
     public var postalCode: String?
     public var phone: String?
     public var partnerDefinedUniqueID: String?
-    public init(firstName: String? = nil, lastName: String? = nil, birthDate: String? = nil, email: String? = nil, postalCode: String? = nil, phone: String? = nil, partnerDefinedUniqueID: String? = nil) {
+    public init(firstName: String? = nil, lastName: String? = nil, birthDate: Date? = nil, email: String? = nil, postalCode: String? = nil, phone: String? = nil, partnerDefinedUniqueID: String? = nil) {
         self.firstName = firstName
         self.lastName = lastName
         self.birthDate = birthDate
