dcd28d5ed248fc2f0ad9ce31b42225b71ac5b583
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-07-02 18:14:08 +0300

    Add new UI option

---
+++ Sources/PrivoSDK/helpers/GlobalOption.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by alex slobodeniuk on 02.07.2021.
+//
+
+import SwiftUI
+
+func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
