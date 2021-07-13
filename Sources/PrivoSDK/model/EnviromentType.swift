20a88888a5e0d0bed385b1873c9bce07a3744620
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-07-13 18:24:06 +0300

    Modify EnviromentType

--- Sources/PrivoSDK/model/EnviromentType.swift
+++ Sources/PrivoSDK/model/EnviromentType.swift
@@ -5,7 +5,7 @@
 //  Created by alex slobodeniuk on 07.06.2021.
 //
 
-public enum EnviromentType: Int, Equatable {
+public enum EnviromentType: Int, Equatable, CaseIterable {
     case Local = 0
     case Dev
     case Int
