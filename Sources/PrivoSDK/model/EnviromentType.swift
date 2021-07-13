51036087ef11f51591d269a141bb70b3e10edf94
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-07-13 16:36:49 +0300

    Fix env enum type

--- Sources/PrivoSDK/model/EnviromentType.swift
+++ Sources/PrivoSDK/model/EnviromentType.swift
@@ -5,8 +5,8 @@
 //  Created by alex slobodeniuk on 07.06.2021.
 //
 
-public enum EnviromentType {
-    case Local
+public enum EnviromentType: Int, Equatable {
+    case Local = 0
     case Dev
     case Int
     case Test
