0cd279f7faec4dd68775dd5da19f73e9c205b5f6
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-07-16 13:11:49 +0300

    Split age gate class to 2 parts

--- Sources/PrivoSDK/PrivoAgeGate.swift
+++ Sources/PrivoSDK/PrivoAgeGate.swift
@@ -8,71 +8,38 @@ import Foundation
 import UIKit
 
 public class PrivoAgeGate {
-    private let AG_ID = "privoAgId";
-    private let FP_ID = "privoFpId";
     private let dateFormatter = DateFormatter()
+    private let ageGate = InternalAgeGate()
 
     public init() {
         dateFormatter.dateFormat = "yyyy-MM-dd"
     }
     
-    private func getFpId(completionHandler: @escaping (String?) -> Void) {
-        if let fpId = UserDefaults.standard.string(forKey: FP_ID) {
-            completionHandler(fpId)
-        } else {
-            if let fingerprint = try? DeviceFingerprint() {
-                PrivoInternal.rest.generateFingerprint(fingerprint: fingerprint) { r in
-                    if let id = r?.id {
-                        UserDefaults.standard.set(id, forKey: self.FP_ID)
-                    }
-                    completionHandler(r?.id)
-                }
-            } else {
-                completionHandler(nil)
-            }
-        }
-    }
-    private func getFpStatus(extUserId: String? = nil, countryCode: String? = nil, completionHandler: @escaping (AgeGateStatus?) -> Void) {
-        getFpId() { fpId in
-            if let fpId = fpId {
-                let record = FpStatusRecord(serviceIdentifier: PrivoInternal.settings.serviceIdentifier, fpId: fpId, birthDate: nil, extUserId: extUserId, countryCode: countryCode)
-                PrivoInternal.rest.processFpStatus(data: record) { r in
-                    if let id = r?.ageGateIdentifier {
-                        UserDefaults.standard.set(id, forKey: self.AG_ID)
-                    }
-                    completionHandler(r)
-                }
-            } else {
-                completionHandler(nil)
-            }
-        }
-    }
-    
     public func getAgeStatus(extUserId: String? = nil, countryCode: String? = nil, completionHandler: @escaping (AgeGateStatus?) -> Void) {
-        if let agId = UserDefaults.standard.string(forKey: AG_ID) {
+        if let agId = UserDefaults.standard.string(forKey: ageGate.AG_ID) {
             let record = AgStatusRecord(serviceIdentifier: PrivoInternal.settings.serviceIdentifier, agId: agId, extUserId: extUserId, countryCode: countryCode)
-            PrivoInternal.rest.processAgStatus(data: record) { r in
-                if let id = r?.ageGateIdentifier {
-                    UserDefaults.standard.set(id, forKey: self.AG_ID)
+            PrivoInternal.rest.processAgStatus(data: record) { [weak self] r in
+                let id = r?.ageGateIdentifier
+                self?.ageGate.storeAgId(id)
+                if (id != nil) {
                     completionHandler(r)
                 } else {
-                    UserDefaults.standard.removeObject(forKey: self.AG_ID)
-                    completionHandler(nil)
+                    self?.ageGate.getFpStatus(extUserId: extUserId,countryCode: countryCode,completionHandler: completionHandler)
                 }
             }
         } else {
-            getFpStatus(extUserId: extUserId,countryCode: countryCode,completionHandler: completionHandler)
+            ageGate.getFpStatus(extUserId: extUserId,countryCode: countryCode,completionHandler: completionHandler)
         }
     }
     
     public func getAgeStatusByBirthDate(birthDate: Date, extUserId: String? = nil, countryCode: String? = nil, completionHandler: @escaping (AgeGateStatus?) -> Void) {
         let textDate = dateFormatter.string(from: birthDate);
-        getFpId() { fpId in
+        ageGate.getFpId() { fpId in
             if let fpId = fpId {
                 let record = FpStatusRecord(serviceIdentifier: PrivoInternal.settings.serviceIdentifier, fpId: fpId, birthDate: textDate, extUserId: extUserId, countryCode: countryCode)
-                PrivoInternal.rest.processBirthDate(data: record) { r in
+                PrivoInternal.rest.processBirthDate(data: record) { [weak self] r in
                     if let id = r?.ageGateIdentifier {
-                        UserDefaults.standard.set(id, forKey: self.AG_ID)
+                        self?.ageGate.storeAgId(id)
                     }
                     completionHandler(r)
                 }
@@ -83,3 +50,55 @@ public class PrivoAgeGate {
 
     }
 }
+
+fileprivate class InternalAgeGate {
+    fileprivate let AG_ID = "privoAgId";
+    fileprivate let FP_ID = "privoFpId";
+    
+    fileprivate func getFpId(completionHandler: @escaping (String?) -> Void) {
+        if let fpId = UserDefaults.standard.string(forKey: FP_ID) {
+            completionHandler(fpId)
+        } else {
+            if let fingerprint = try? DeviceFingerprint() {
+                PrivoInternal.rest.generateFingerprint(fingerprint: fingerprint) { [weak self] r in
+                    if let id = r?.id {
+                        self?.storeFpId(id)
+                    }
+                    completionHandler(r?.id)
+                }
+            } else {
+                completionHandler(nil)
+            }
+        }
+    }
+    fileprivate func getFpStatus(extUserId: String? = nil, countryCode: String? = nil, completionHandler: @escaping (AgeGateStatus?) -> Void) {
+        getFpId() { fpId in
+            if let fpId = fpId {
+                let record = FpStatusRecord(serviceIdentifier: PrivoInternal.settings.serviceIdentifier, fpId: fpId, birthDate: nil, extUserId: extUserId, countryCode: countryCode)
+                PrivoInternal.rest.processFpStatus(data: record) { [weak self] r in
+                    if let id = r?.ageGateIdentifier {
+                        self?.storeAgId(id)
+                    }
+                    completionHandler(r)
+                }
+            } else {
+                completionHandler(nil)
+            }
+        }
+    }
+    fileprivate func storeAgId(_ id: String?) {
+        if let id = id {
+            UserDefaults.standard.set(id, forKey: self.AG_ID)
+        } else {
+            UserDefaults.standard.removeObject(forKey: self.AG_ID)
+        }
+    }
+    fileprivate func storeFpId(_ id: String?) {
+        if let id = id {
+            UserDefaults.standard.set(id, forKey: self.FP_ID)
+        } else {
+            UserDefaults.standard.removeObject(forKey: self.FP_ID)
+        }
+    }
+}
+
