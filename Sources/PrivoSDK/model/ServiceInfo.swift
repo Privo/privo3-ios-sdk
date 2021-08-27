8cc2fba5c83241b11c385bd1f104c27340d339ff
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-08-27 15:02:57 +0300

    fix ServiceInfo types

--- Sources/PrivoSDK/model/ServiceInfo.swift
+++ Sources/PrivoSDK/model/ServiceInfo.swift
@@ -10,6 +10,6 @@ import Foundation
 struct ServiceInfo: Decodable {
     let serviceIdentifier: String
     let apiKeys: Array<String>?
-    let authMethods: Array<String>?
+    let authMethods: Array<0>?
     let p2siteId: Int?
 }
