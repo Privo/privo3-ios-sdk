d3531f2a8ac72081db2e046866056323630b1e9c
AlexSlo <aslobodeniuk@swiftinvention.com>
2021-11-08 12:17:14 +0300

    Modify keychain

--- Sources/PrivoSDK/helpers/PrivoKeychain.swift
+++ Sources/PrivoSDK/helpers/PrivoKeychain.swift
@@ -12,13 +12,13 @@ struct PrivoKeychain {
     private let privoPrefix = "com.privo.ios.sdk"
     
     func set (key: String, value: String) -> Bool {
+        delete(key);
         if let data = value.data(using: String.Encoding.utf8) {
-            let prefixedKey = keyWithPrefix(key)
             let query: [String : Any] = [
-                kSecClass as String       : kSecClassGenericPassword,
-                kSecAttrAccount as String : prefixedKey,
-                kSecValueData as String   : data,
-                kSecAttrAccessibleWhenUnlocked as String  : kSecAttrAccessibleWhenUnlocked
+                kSecClass as String         : kSecClassGenericPassword,
+                kSecAttrService as String : privoPrefix,
+                kSecAttrAccount as String   : key,
+                kSecValueData as String     : data
             ]
                   
             let result = SecItemAdd(query as CFDictionary, nil)
@@ -28,11 +28,11 @@ struct PrivoKeychain {
     }
     
     func get(_ key: String) -> String? {
-        let prefixedKey = keyWithPrefix(key)
         
         let query: [String: Any] = [
             kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : prefixedKey,
+            kSecAttrService as String : privoPrefix,
+            kSecAttrAccount as String : key,
             kSecMatchLimit as String  : kSecMatchLimitOne,
             kSecReturnData as String: kCFBooleanTrue!
         ]
@@ -53,7 +53,16 @@ struct PrivoKeychain {
         return nil;
     }
     
-    private func keyWithPrefix(_ key: String) -> String {
-        return "\(privoPrefix).\(key)"
+    func delete(_ key: String) -> Bool {
+      
+      let query: [String: Any] = [
+        kSecClass as String       : kSecClassGenericPassword,
+        kSecAttrService as String : privoPrefix,
+        kSecAttrAccount as String : key,
+      ]
+      
+      let response = SecItemDelete(query as CFDictionary)
+      
+      return response == noErr
     }
 }
