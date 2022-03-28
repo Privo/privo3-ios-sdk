54bb949a1099b1cd1af4d1917178e85961881874
AlexSlo <alexslo9219@gmail.com>
2022-03-28 12:47:58 +0300

    Fix port in age gate config

--- Sources/PrivoSDK/Configuration.swift
+++ Sources/PrivoSDK/Configuration.swift
@@ -26,7 +26,7 @@ class Configuration {
             self.verificationUrl = URL(string: "https://verification-dev.privo.com/vw")!
             self.helperUrl = URL(string: "https://helper-svc-dev.privo.com/rest/api/v1.0")!
             self.lgsRegistrationUrl = URL(string: "https://privohub-dev.privo.com/lgs")!
-            self.ageGateUrl = URL(string: "https://agegate-dev.privo.com:443/api/v1.0/age-gate")!
+            self.ageGateUrl = URL(string: "https://agegate-dev.privo.com/api/v1.0/age-gate")!
             self.tokenStorageKey = "privo-token-local"
         case .Dev:
             self.authBaseUrl = URL(string: "https://auth-dev.privo.com/api/v1.0")!
@@ -34,7 +34,7 @@ class Configuration {
             self.verificationUrl = URL(string: "https://verification-dev.privo.com/vw")!
             self.helperUrl = URL(string: "https://helper-svc-dev.privo.com/rest/api/v1.0")!
             self.lgsRegistrationUrl = URL(string: "https://privohub-dev.privo.com/lgs")!
-            self.ageGateUrl = URL(string: "https://agegate-dev.privo.com:443/api/v1.0/age-gate")!
+            self.ageGateUrl = URL(string: "https://agegate-dev.privo.com/api/v1.0/age-gate")!
             self.tokenStorageKey = "privo-token-dev"
         case .Int:
             self.authBaseUrl = URL(string: "https://auth-int.privo.com/api/v1.0")!
@@ -42,7 +42,7 @@ class Configuration {
             self.verificationUrl = URL(string: "https://verification-int.privo.com/vw")!
             self.helperUrl = URL(string: "https://helper-svc-int.privo.com/rest/api/v1.0")!
             self.lgsRegistrationUrl = URL(string: "https://privohub-int.privo.com/lgs")!
-            self.ageGateUrl = URL(string: "https://agegate-int.privo.com:443/api/v1.0/age-gate")!
+            self.ageGateUrl = URL(string: "https://agegate-int.privo.com/api/v1.0/age-gate")!
             self.tokenStorageKey = "privo-token-int"
         case .Test:
             self.authBaseUrl = URL(string: "https://auth-test.privo.com/api/v1.0")!
@@ -50,7 +50,7 @@ class Configuration {
             self.verificationUrl = URL(string: "https://verification-test.privo.com/vw")!
             self.helperUrl = URL(string: "https://helper-svc-test.privo.com/rest/api/v1.0")!
             self.lgsRegistrationUrl = URL(string: "https://privohub-test.privo.com/lgs")!
-            self.ageGateUrl = URL(string: "https://agegate-test.privo.com:443/api/v1.0/age-gate")!
+            self.ageGateUrl = URL(string: "https://agegate-test.privo.com/api/v1.0/age-gate")!
             self.tokenStorageKey = "privo-token-test"
         case .Prod:
             self.authBaseUrl = URL(string: "https://auth.privo.com/api/v1.0")!
@@ -58,7 +58,7 @@ class Configuration {
             self.verificationUrl = URL(string: "https://verification.privo.com/vw")!
             self.helperUrl = URL(string: "https://helper-svc.privo.com/rest/api/v1.0")!
             self.lgsRegistrationUrl = URL(string: "https://privohub.privo.com/lgs")!
-            self.ageGateUrl = URL(string: "https://agegate.privo.com:443/api/v1.0/age-gate")!
+            self.ageGateUrl = URL(string: "https://agegate.privo.com/api/v1.0/age-gate")!
             self.tokenStorageKey = "privo-token"
         }
     }
