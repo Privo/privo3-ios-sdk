3dca64da4a98cd976d48041308228806f76da045
AlexSlo <aslobodeniuk@swiftinvention.com>
2022-08-29 16:02:55 +0300

    Clean up age-gate storage in case of undef event

--- Sources/PrivoSDK/internal/PrivoAgeGateInternal.swift
+++ Sources/PrivoSDK/internal/PrivoAgeGateInternal.swift
@@ -26,7 +26,10 @@ internal class PrivoAgeGateInternal {
         };
         
         if let event = event {
-            if (event.status != AgeGateStatus.Canceled) {
+            if (event.status != AgeGateStatus.Undefined) {
+                let key = "\(self.AGE_EVENT_KEY_PREFIX)-\(event.userIdentifier ?? "")"
+                self.keychain.delete(key)
+            } else if (event.status != AgeGateStatus.Canceled) {
                 serviceSettings.getSettings { [weak self] settings in
                     let interval = Double(settings.poolAgeGateStatusInterval)
                     let expireEvent = AgeGateExpireEvent(event: event, expires: getEventExpiration(interval))
